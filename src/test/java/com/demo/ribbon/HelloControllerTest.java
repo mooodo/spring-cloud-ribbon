96f6799b76e84b6acb6cf37371d0385ae60b0bad
fangang <fangang@DESKTOP-7DODCNH>
2020-05-19 14:11:22 +0800

    add tests with springcloud contract

--- src/test/java/com/demo/ribbon/HelloControllerTest.java
+++ src/test/java/com/demo/ribbon/HelloControllerTest.java
@@ -3,8 +3,8 @@
  */
 package com.demo.ribbon;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,25 +15,37 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
-
+import org.springframework.test.web.servlet.MockMvc;
 
 /**
  * @author fangang
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes=RibbonApplication.class, webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
 @AutoConfigureStubRunner(ids= {"com.demo:spring-cloud-service:+:stubs:8762"}, stubsMode = StubRunnerProperties.StubsMode.LOCAL)
 public class HelloControllerTest {
 	@Autowired
-	RestTemplate restTemplate;
+	MockMvc mvc;
 	@Test
-	public void testSayHello() {
-		String url = "http://localhost:8762/sayHello?user= {user}";
-		String user = "Johnwood";
-		String actual = restTemplate.getForObject(url, String.class, user);
-		assertThat(actual, equalTo("Hi Johnwood. The server port is 8762"));
+	public void testSayHello() throws Exception {
+		mvc.perform(get("/ribbon/sayHello").param("user", "Johnwood"))
+			.andExpect(status().isOk())
+			.andExpect(content().string("Hi Johnwood, welcome to you! The server port is null"));
+	}
+	
+	@Test
+	public void testShowMe() throws Exception {
+		mvc.perform(get("/ribbon/showMe"))
+			.andExpect(status().isOk())
+			.andExpect(content().json("{'id':0,'name':'Mooodo','gender':'male'}"));
+	}
+	
+	@Test
+	public void testFindPerson() throws Exception {
+		mvc.perform(get("/ribbon/findPerson?name={name}&gender={gender}", "Mooodo","male"))
+			.andExpect(status().isOk())
+			.andExpect(content().json("{'id':0,'name':'Mooodo','gender':'male'}"));
 	}
 }
